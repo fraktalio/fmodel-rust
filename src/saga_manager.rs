7ef6d436a8077ed3128de920d3d16297e6007641
Ivan Dugalic <idugalic@gmail.com>
2023-12-02 23:19:12 +0100

    ActionComputation trait added. Saga as implementation.

--- src/saga_manager.rs
+++ src/saga_manager.rs
@@ -2,7 +2,7 @@ use std::marker::PhantomData;
 
 use async_trait::async_trait;
 
-use crate::saga::Saga;
+use crate::saga::ActionComputation;
 
 /// Publishes the action/command to some external system.
 ///
@@ -26,21 +26,23 @@ pub trait ActionPublisher<A, Error> {
 /// - `AR` - Action Result / Event
 /// - `Publisher` - Action Publisher
 /// - `Error` - Error
-pub struct SagaManager<'a, A, AR, Publisher, Error>
+pub struct SagaManager<A, AR, Publisher, Saga, Error>
 where
     Publisher: ActionPublisher<A, Error>,
+    Saga: ActionComputation<AR, A>,
 {
     action_publisher: Publisher,
-    saga: Saga<'a, AR, A>,
+    saga: Saga,
     _marker: PhantomData<(A, AR, Error)>,
 }
 
-impl<'a, A, AR, Publisher, Error> SagaManager<'a, A, AR, Publisher, Error>
+impl<A, AR, Publisher, Saga, Error> SagaManager<A, AR, Publisher, Saga, Error>
 where
     Publisher: ActionPublisher<A, Error>,
+    Saga: ActionComputation<AR, A>,
 {
     /// Creates a new instance of [SagaManager].
-    pub fn new(action_publisher: Publisher, saga: Saga<'a, AR, A>) -> Self {
+    pub fn new(action_publisher: Publisher, saga: Saga) -> Self {
         SagaManager {
             action_publisher,
             saga,
@@ -49,7 +51,7 @@ where
     }
     /// Handles the action result by publishing it to the external system.
     pub async fn handle(&self, action_result: &AR) -> Result<Vec<A>, Error> {
-        let new_actions = (self.saga.react)(action_result);
+        let new_actions = self.saga.compute_new_actions(action_result);
         let published_actions = self.action_publisher.publish(&new_actions).await?;
         Ok(published_actions)
     }
