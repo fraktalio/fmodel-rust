7085ea286daeebb1ac721cdf99a7684dd6f48a47
Ivan Dugalic <idugalic@gmail.com>
2023-12-14 22:17:31 +0100

    implementing delegation pattern on the application layer components.
    Application layer components are composed out of effects (fetch. save) and pure computation. We want to make this obvious.

--- src/saga_manager.rs
+++ src/saga_manager.rs
@@ -36,10 +36,41 @@ where
     _marker: PhantomData<(A, AR, Error)>,
 }
 
-impl<A, AR, Publisher, Saga, Error> SagaManager<A, AR, Publisher, Saga, Error>
+impl<A, AR, Publisher, Saga, Error> ActionComputation<AR, A>
+    for SagaManager<A, AR, Publisher, Saga, Error>
 where
     Publisher: ActionPublisher<A, Error>,
     Saga: ActionComputation<AR, A>,
+{
+    /// Computes new actions based on the action result.
+    fn compute_new_actions(&self, action_result: &AR) -> Vec<A> {
+        self.saga.compute_new_actions(action_result)
+    }
+}
+
+#[async_trait]
+impl<A, AR, Publisher, Saga, Error> ActionPublisher<A, Error>
+    for SagaManager<A, AR, Publisher, Saga, Error>
+where
+    Publisher: ActionPublisher<A, Error> + Sync,
+    Saga: ActionComputation<AR, A> + Sync,
+    A: Sync,
+    AR: Sync,
+    Error: Sync,
+{
+    /// Publishes the action/command to some external system, returning either the actions that are successfully published or error.
+    async fn publish(&self, action: &[A]) -> Result<Vec<A>, Error> {
+        self.action_publisher.publish(action).await
+    }
+}
+
+impl<A, AR, Publisher, Saga, Error> SagaManager<A, AR, Publisher, Saga, Error>
+where
+    Publisher: ActionPublisher<A, Error> + Sync,
+    Saga: ActionComputation<AR, A> + Sync,
+    A: Sync,
+    AR: Sync,
+    Error: Sync,
 {
     /// Creates a new instance of [SagaManager].
     pub fn new(action_publisher: Publisher, saga: Saga) -> Self {
@@ -49,10 +80,13 @@ where
             _marker: PhantomData,
         }
     }
-    /// Handles the action result by publishing it to the external system.
+    /// Handles the `action result` by computing new `actions` based on `action result`, and publishing new `actions` to the external system.
+    /// In most cases:
+    ///  - the `action result` is an `event` that you react,
+    ///  - the `actions` are `commands` that you publish downstream.
     pub async fn handle(&self, action_result: &AR) -> Result<Vec<A>, Error> {
-        let new_actions = self.saga.compute_new_actions(action_result);
-        let published_actions = self.action_publisher.publish(&new_actions).await?;
+        let new_actions = self.compute_new_actions(action_result);
+        let published_actions = self.publish(&new_actions).await?;
         Ok(published_actions)
     }
 }
