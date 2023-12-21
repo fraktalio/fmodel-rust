e7200aa929b0759cabd8db34d3a28c7575698e9c
Ivan Dugalic <idugalic@gmail.com>
2023-12-21 19:10:55 +0100

    Orchestrating State Stored Aggregate added.

--- src/aggregate.rs
+++ src/aggregate.rs
@@ -2,7 +2,8 @@ use std::marker::PhantomData;
 
 use async_trait::async_trait;
 
-use crate::decider::{EventComputation, StateComputation};
+use crate::decider::{Decider, EventComputation, StateComputation};
+use crate::saga::{ActionComputation, Saga};
 
 /// Event Repository trait
 ///
@@ -210,6 +211,117 @@ where
         StateStoredAggregate {
             repository,
             decider,
+            _marker: PhantomData,
+        }
+    }
+    /// Handles the command by fetching the state from the repository, computing new state based on the current state and the command, and saving the new state to the repository.
+    pub async fn handle(&self, command: &C) -> Result<(S, Version), Error> {
+        let state_version = self.fetch_state(command).await?;
+        match state_version {
+            None => {
+                let new_state = self.compute_new_state(None, command);
+                let saved_state = self.save(&new_state, &None).await?;
+                Ok(saved_state)
+            }
+            Some((state, version)) => {
+                let new_state = self.compute_new_state(Some(state), command);
+                let saved_state = self.save(&new_state, &Some(version)).await?;
+                Ok(saved_state)
+            }
+        }
+    }
+}
+
+/// Orchestrating State Stored Aggregate.
+///
+/// It is using a [Decider] and [Saga] to compute new state based on the current state and the command.
+/// If the `decider` is combined out of many deciders via `combine` function, a `saga` could be used to react on new events and send new commands to the `decider` recursively, in single transaction.
+/// It is using a [StateRepository] to fetch the current state and to save the new state.
+///
+/// Generic parameters:
+///
+/// - `C` - Command
+/// - `S` - State
+/// - `E` - Event
+/// - `Repository` - State repository
+/// - `Version` - Version
+/// - `Error` - Error
+pub struct StateStoredOrchestratingAggregate<'a, C, S, E, Repository, Version, Error>
+where
+    Repository: StateRepository<C, S, Version, Error>,
+{
+    repository: Repository,
+    decider: Decider<'a, C, S, E>,
+    saga: Saga<'a, E, C>,
+    _marker: PhantomData<(C, S, E, Version, Error)>,
+}
+
+impl<'a, C, S, E, Repository, Version, Error> StateComputation<C, S, E>
+    for StateStoredOrchestratingAggregate<'a, C, S, E, Repository, Version, Error>
+where
+    Repository: StateRepository<C, S, Version, Error>,
+    S: Clone,
+{
+    /// Computes new state based on the current state and the command.
+    fn compute_new_state(&self, current_state: Option<S>, command: &C) -> S {
+        let effective_current_state =
+            current_state.unwrap_or_else(|| (self.decider.initial_state)());
+        let events = (self.decider.decide)(command, &effective_current_state);
+        let mut new_state = events.iter().fold(effective_current_state, |state, event| {
+            (self.decider.evolve)(&state, event)
+        });
+        let commands = events
+            .iter()
+            .flat_map(|event: &E| self.saga.compute_new_actions(event))
+            .collect::<Vec<C>>();
+        commands.iter().for_each(|action| {
+            new_state = self.compute_new_state(Some(new_state.clone()), action);
+        });
+        new_state
+    }
+}
+
+#[async_trait]
+impl<'a, C, S, E, Repository, Version, Error> StateRepository<C, S, Version, Error>
+    for StateStoredOrchestratingAggregate<'a, C, S, E, Repository, Version, Error>
+where
+    Repository: StateRepository<C, S, Version, Error> + Sync,
+    C: Sync,
+    S: Sync,
+    E: Sync,
+    Version: Sync,
+    Error: Sync,
+{
+    /// Fetches current state, based on the command.
+    async fn fetch_state(&self, command: &C) -> Result<Option<(S, Version)>, Error> {
+        self.repository.fetch_state(command).await
+    }
+    /// Saves state.
+    async fn save(&self, state: &S, version: &Option<Version>) -> Result<(S, Version), Error> {
+        self.repository.save(state, version).await
+    }
+}
+
+impl<'a, C, S, E, Repository, Version, Error>
+    StateStoredOrchestratingAggregate<'a, C, S, E, Repository, Version, Error>
+where
+    Repository: StateRepository<C, S, Version, Error> + Sync,
+    C: Sync,
+    S: Sync + Clone,
+    E: Sync,
+    Version: Sync,
+    Error: Sync,
+{
+    /// Creates a new instance of [StateStoredAggregate].
+    pub fn new(
+        repository: Repository,
+        decider: Decider<'a, C, S, E>,
+        saga: Saga<'a, E, C>,
+    ) -> Self {
+        StateStoredOrchestratingAggregate {
+            repository,
+            decider,
+            saga,
             _marker: PhantomData,
         }
     }
